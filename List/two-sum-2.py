3f90c22a54e488e1e765c7f5ac3f877059448ea0
Sion Noh <shtldhs123@ajou.ac.kr>
2022-03-21 20:28:43 +0900

    두 수의 합 - 투 포인터

---
+++ List/two-sum-2.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/two-sum
+# 투 포인터 이용 - 인덱스가 아닌 값을 찾아내는 문제에만 적용 가능
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    nums.sort()
+    left, right = 0, len(nums)-1
+    while not left == right:
+        if nums[left] + nums[right] < target:
+            left += 1
+        elif nums[left] + nums[right] > target:
+            right -= 1
+        else:
+            return [nums[left], nums[right]]
+
+print(twoSum([2, 15, 11, 7], 22))
